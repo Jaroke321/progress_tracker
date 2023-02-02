538fc6ab4853ced62f7de615e3b41ba2acfd1b7b
Jake Keller <jakekeller@Jakes-Mac-mini.lan>
2023-02-02 18:17:34 -0500

    Got PyQt6 installed and a simple window opening

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import optparse
+#!/usr/bin/python
 
-def main():
-    print("Hello World")
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import pyqtSlot
 
+def window():
+   
+   app = QApplication(sys.argv)
 
-if __name__ == "__main__":
-    main()
+   # Create a Qt widget, which will be our window.
+   window = QWidget()
+   window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+   # Start the event loop.
+   app.exec()
+
+if __name__ == '__main__':
+   window()
